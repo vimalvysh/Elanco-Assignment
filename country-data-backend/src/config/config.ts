3d276c92d7bc75ff9828a432d07a705361274a03
vimalvysh <vimalvyshagam@gmail.com>
2025-01-21 22:33:01 +0530

    added config

---
+++ country-data-backend/src/config/config.ts
@@ -0,0 +1,1 @@
+export const REST_COUNTRIES_API = 'https://restcountries.com/v3.1/all';
--- country-data-backend/src/index.ts
+++ country-data-backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import 'express-async-errors';
+// Project dependencies
 import countryRoutes from './routes/countryRoutes';
+import { errorHandler } from './middlewares/error';
 
 const app = express();
 const PORT = 3001;
@@ -10,6 +13,8 @@ app.use(express.json());
 
 // Routes
 app.use('/countries', countryRoutes);
+// Error handling
+app.use(errorHandler);
 
 // Start server
 app.listen(PORT, () => {
